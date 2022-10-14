c463d8ec0e2c767266adb7c5bcb8dab9a2dad67d
my0990 <my0990@naver.com>
2022-10-14 16:31:30 +0900

    timer state

--- src/components/timer/TimerTemplate.js
+++ src/components/timer/TimerTemplate.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components";
 import { useTimer } from 'react-timer-hook';
+import { useState, useMemo } from 'react';
 
 const TimerTemplateContainer = styled.div`
 
@@ -16,23 +17,46 @@ const TimerTemplateContainer = styled.div`
 function TimerTemplate() {
 
     const expiryTimestamp = new Date();
-    expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 3);
-
+    expiryTimestamp.setSeconds(expiryTimestamp.getSeconds());
     const {
       seconds,
       minutes,
       hours,
       days,
-      isRunning,                                       
+      isRunning,
       start,
       pause,
       resume,
       restart,
-    } = useTimer({expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+    } = useTimer({expiryTimestamp});
+    const [plusMinute,setPlusMinute] = useState(0);
+    let time = useMemo(()=> new Date(),[]);
+    
+    
+    
+    const setMinute = (number) => {
+        if(!isRunning){
+            time = new Date();
+            setPlusMinute((num) => num + parseInt(number));
+            time.setSeconds(time.getSeconds() + plusMinute);
+        }
+        time.setSeconds(time.getSeconds() + number);
+        restart(time);
+        pause();
+
+    }
+
     return(
         <TimerTemplateContainer>
             <div style={{fontSize: '100px'}} className="timeDisplay">
                 <span>{String(hours).padStart(2,'0')}</span>:<span>{String(minutes).padStart(2,'0')}</span>:<span>{String(seconds).padStart(2,'0')}</span>
+            </div>
+            <div>
+                <button onClick={() => setMinute(60)}>1분</button>
+                <button onClick={() => setMinute(180)}>3분</button>
+                <button onClick={() => setMinute(300)}>5분</button>
+                <button onClick={() => setMinute(600)}>10분</button>
+         
             </div>
             <div>
                 <button onClick={start}>Start</button>
