81d34069e41e2916d5b2e1ce779da8aaeda73d80
my0990 <my0990@naver.com>
2022-10-16 15:06:39 +0900

    timer onexpire event

--- src/components/timer/TimerTemplate.js
+++ src/components/timer/TimerTemplate.js
@@ -4,8 +4,6 @@ import { useTimer } from 'react-timer-hook';
 import { useState, useMemo } from 'react';
 
 const TimerTemplateContainer = styled.div`
-
-    // width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
@@ -16,7 +14,7 @@ const TimerTemplateContainer = styled.div`
 
 function TimerTemplate() {
 
-    const expiryTimestamp = new Date();
+    const expiryTimestamp = useMemo(()=> new Date(),[]);
     expiryTimestamp.setSeconds(expiryTimestamp.getSeconds());
     const {
       seconds,
@@ -29,43 +27,47 @@ function TimerTemplate() {
       resume,
       restart,
     } = useTimer({expiryTimestamp});
-    const [plusMinute,setPlusMinute] = useState(0);
-    let time = useMemo(()=> new Date(),[]);
+    const [isStart,setIsStart] = useState(false);
+
     const setMinute = (number) => {
-        let tmp = new Date();
-        if(isRunning){
-            time.setSeconds(time.getSeconds() + number);
+        var current = new Date();
+        if(!isRunning){
+            expiryTimestamp.setTime(current.getTime());
         }
-
-        
-        restart(time)
-
+        expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + number);
+        restart(expiryTimestamp)
+    }
+    const onReset = () => {
+        var current = new Date();
+        expiryTimestamp.setTime(current.getTime());
+        setIsStart(false);
+        restart(expiryTimestamp);
+    }
+    const onStart = () => {
+        setIsStart(true);
+        resume();
+    }
+    const onPause = () => {
+        setIsStart(false);
+        pause();
     }
 
     return(
         <TimerTemplateContainer>
             <div style={{fontSize: '100px'}} className="timeDisplay">
                 <span>{String(hours).padStart(2,'0')}</span>:<span>{String(minutes).padStart(2,'0')}</span>:<span>{String(seconds).padStart(2,'0')}</span>
-                <div>
-                {plusMinute}
-                </div>
             </div>
             <div>
-                <button onClick={() => setMinute(60)}>1분</button>
+                <button onClick={() => setMinute(1)}>1분</button>
                 <button onClick={() => setMinute(180)}>3분</button>
                 <button onClick={() => setMinute(300)}>5분</button>
                 <button onClick={() => setMinute(600)}>10분</button>
-                <button onClick={()=>{console.log(time)}}>test</button>
+                {/* <button onClick={() => console.log(time)}>test</button> */}
             </div>
             <div>
-                <button onClick={resume}>start</button> 
-                <button onClick={pause}>Pause</button> 
-                <button onClick={() => {
-                    // Restarts to 5 minutes timer
-                    const time = new Date();
-                    time.setSeconds(time.getSeconds());
-                    restart(time)
-                }}>Restart</button>
+                <button onClick={onStart}>start</button> 
+                <button onClick={onPause}>Pause</button> 
+                <button onClick={onReset}>reset</button>
             </div>
             {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
             {/* <button onClick={start}>Start</button>
