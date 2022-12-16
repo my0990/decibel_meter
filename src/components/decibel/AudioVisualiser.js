547465d054de2a270d5b3d1cd6aa1f10fc42c619
my0990 <my0990@naver.com>
2022-12-16 19:59:33 +0900

    222

--- src/components/decibel/AudioVisualiser.js
+++ src/components/decibel/AudioVisualiser.js
@@ -9,6 +9,8 @@ const AudioVisualiser = ({audioData}) => {
         overflow: hidden;
         h1 {margin: 0}
     `
+
+    const analyserCanvas = useRef(null);
     const [audio,setAudio] = useState(null);
     // 오디오 미디어 스트림 가져오고 스테이트에 저장
     const getMicrophone = async () => {
@@ -29,15 +31,56 @@ const AudioVisualiser = ({audioData}) => {
         } else {
             getMicrophone();
         }
-        console.log('audio');
     }
 
-    const canvasRef = useRef(null);
+    useEffect(()=>{
+        if(audio){
+            try{
+                const audioCtx = new AudioContext();
+                const analyser = audioCtx.createAnalyser();
+                analyser.fftSize = 2048;
+                const audioSrc = audioCtx.createMediaStreamSource(audio);
+                audioSrc.connect(analyser);
+                const data = new Uint8Array(analyser.frequencyBinCount);
+    
+                
+                const ctx = analyserCanvas.current.getContext('2d');
+                
+                analyserCanvas.height = window.innerHeight;
+                analyserCanvas.width = window.innerWidth;
+                const draw = dataParm => {
+                  ctx.fillStyle = 'white';
+                  ctx.fillRect(0,0,analyserCanvas.current.width,analyserCanvas.current.height)
+                  ctx.lineWidth = 2;
+                  ctx.strokeStyle = 'blue';
+                  const space = analyserCanvas.current.width / dataParm.length;
+                  dataParm.forEach((value, i) => {
+                    ctx.beginPath();
+                    ctx.moveTo(space * i, analyserCanvas.current.height);
+                    ctx.lineTo(space * i, analyserCanvas.current.height - value*0.5);
+                    ctx.stroke();
+                  }
+                  );
+                }
 
-    const draw = () => {
-        console.log(audioData);
+                const intervalId = setInterval(()=>{
+                  analyser.getByteFrequencyData(data);
+                  draw(data);
+                },1000);
+                return () => {
+                  clearInterval(intervalId);
+                }
+              } catch(err){
+                throw(500,err);
+              }
+            }
+    },[audio]);
+    
+
+    // const draw = () => {
+    //     console.log(audio);
         
-        const canvas = canvasRef.current;
+    //     const canvas = canvasRef.current;
         // const height = canvas.height;
         // const width = canvas.width;
         // const context = canvas.getContext('2d');
@@ -59,7 +102,7 @@ const AudioVisualiser = ({audioData}) => {
 
         // context.lineTo(x, height/2);
         // context.stroke();
-    }
+    // }
     // useEffect(()=>{
     //     const intervalId = setInterval(()=>{
     //         draw();
@@ -74,6 +117,7 @@ const AudioVisualiser = ({audioData}) => {
                     {audio ? 'Stop microphone' : 'Get microphone input'}
                 </button>
             </div>
+            <canvas ref={analyserCanvas} />
         </DecibelTemplateContainer>
     )
 }
